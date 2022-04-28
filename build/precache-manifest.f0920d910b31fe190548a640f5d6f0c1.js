95652d8b7b5445cf320cbeaa0947a0ffef666069
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2022-04-28 14:36:41 -0400

    Building

---
+++ build/precache-manifest.f0920d910b31fe190548a640f5d6f0c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4a0eb2f1435a9a6af00406d3a9ab1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f2d886c3319e914d808",
+    "url": "/static/css/main.020e2537.chunk.css"
+  },
+  {
+    "revision": "751e1bf629144ba2b309",
+    "url": "/static/js/2.7c706a04.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.7c706a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2d886c3319e914d808",
+    "url": "/static/js/main.413140cf.chunk.js"
+  },
+  {
+    "revision": "7873725ba672e91c2fdf",
+    "url": "/static/js/runtime-main.deab8be9.js"
+  }
+]);
