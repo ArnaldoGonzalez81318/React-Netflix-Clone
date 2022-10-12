9795ddf8984825f09ec2cb795ad82aecf616f377
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2022-10-12 13:10:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import SigninPage from "./pages/SigninPage";
+// import SigninPage from "./pages/SigninPage";
 import SignupPage from "./pages/SignupPage";
 import BrowsePage from "./pages/BrowsePage";
 
@@ -12,9 +12,9 @@ function App() {
         <Route exact path="/">
           <HomePage />
         </Route>
-        <Route path="/signin">
+        {/* <Route path="/signin">
           <SigninPage />
-        </Route>
+        </Route> */}
         <Route path="/signup">
           <SignupPage />
         </Route>
