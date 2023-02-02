2262f7e3f8b6a6df576bb5f1b0f41644815d6562
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2023-02-01 22:13:10 -0500

    Update FooterTitle.js

--- src/components/Footer/FooterTitle.js
+++ src/components/Footer/FooterTitle.js
@@ -3,6 +3,7 @@ import "./FooterStyles.css";
 
 function FooterTitle({ children, ...restProps }) {
   return (
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid
     <a href="#" className="footer-title" {...restProps}>
       {children}
     </a>
