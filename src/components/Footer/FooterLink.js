a7fa753474193da6b9acd17c8dbf8eb9a5714245
ArnaldoGonzalez81318 <arnaldolgonzalez96@gmail.com>
2023-02-01 22:12:47 -0500

    Update FooterLink.js

--- src/components/Footer/FooterLink.js
+++ src/components/Footer/FooterLink.js
@@ -3,6 +3,7 @@ import "./FooterStyles.css";
 
 function FooterLink({ children, ...restProps }) {
   return (
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid
     <a href="#" className="footer-link" {...restProps}>
       {children}
     </a>
